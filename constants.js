dc1687f0b98110005ee55817748473a1f47e586f
jnanateja009 <84826756+jnanateja009@users.noreply.github.com>
2021-09-07 10:03:16 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,75 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello","hello ra", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what's happening in Campus","what's up","campus"],
+  ["Tell me about Clubs in Cse Department","clubs","student clubs in jain"],
+  ["what is ignan","ignan"],
+  ["who is our vice chancellor","vice chancellor"],
+  ["who is hod of cse department","hod cse","hod"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what is available in canteen", "menu in canteen"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  ["Classes going on!"],
+  ["Enigma, ignan"],
+  ["A Club for Tech and Fun Actvities"],
+  ["Dr. Raj Singh Sir"],
+  ["Dr Manjunath sir"],
+  ["S N J TEJA"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Soft Drinks", "Pizza", " and many more for you"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Bro...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
